b2cb3385e5323b08d6c709d526986ae35d0c2489
Nishant Kumar <nishantcoder108@gmail.com>
2023-11-11 01:58:48 +0530

    Implement user login functionality with JWT

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
--- packages/backend/src/routes/userRoutes.ts
+++ packages/backend/src/routes/userRoutes.ts
@@ -1,5 +1,7 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/User";
 
 const router = Router();
@@ -8,6 +10,7 @@ router.get("/users", (req, res) => {
   res.send("List of all Users ...");
 });
 
+//Signup
 router.post(
   "/users",
   [body("email").isEmail(), body("password").isLength({ min: 6 })],
@@ -31,11 +34,41 @@ router.post(
 
       // Respond with the created user
       res.status(201).json(newUser);
-    } catch (err) {
+    } catch (err: any) {
       // General error handling
-      res.status(500).json({ message: "Server error", error: err });
+      res.status(500).json({ message: "Server error", error: err.message });
     }
   }
 );
+
+//Login
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "Email does not exist." });
+    }
+
+    // Check if the password is correct
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password." });
+    }
+
+    // User is authenticated, proceed with generating a token/session
+    if (isMatch) {
+      const token = jwt.sign(
+        { userId: user._id }, // Payload
+        process.env.JWT_SECRET as string, // Secret key
+        { expiresIn: "1h" } // Expiration time
+      );
+
+      res.json({ token });
+    }
+  } catch (err: any) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
 
 export default router;
