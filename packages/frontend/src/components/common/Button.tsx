d54c422d7f2a1a4241db82a4fc2202e61c7a9aae
Nishant Kumar <nishantcoder108@gmail.com>
2023-11-15 09:44:31 +0530

    Make common Btn and Navbar

---
+++ packages/frontend/src/components/common/Button.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+// Define the props for the button
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  variant?: "primary" | "secondary" | "gradient";
+  size?: "small" | "medium" | "large";
+  // Add other props as needed
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  variant = "primary",
+  size = "medium",
+}) => {
+  // Class names for different variants and sizes
+  const variantClasses = {
+    primary: "bg-blue-500 hover:bg-blue-700 text-white",
+    secondary: "bg-gray-300 hover:bg-gray-400 text-black",
+    gradient:
+      "bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-500 text-white",
+  };
+
+  const sizeClasses = {
+    small: "text-sm px-2 py-1",
+    medium: "text-md px-3 py-1",
+    large: "text-lg px-6 py-3",
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded ${variantClasses[variant]} ${sizeClasses[size]} focus:outline-none`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- packages/frontend/src/components/layout/Navbar.tsx
+++ packages/frontend/src/components/layout/Navbar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from "../../redux/slices/themeSlice";
 import { RootState } from "../../redux/store";
+import { Link } from "react-router-dom";
+import Button from "../common/Button";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,31 +14,39 @@ const Navbar: React.FC = () => {
   return (
     <div>
       <nav
-        className={`bg-gray-800 text-white ${
+        className={`bg-gray-800 text-white py-1 ${
           theme === "dark" ? "bg-gray-800 " : "bg-[#f2eeee91] text-gray-800"
         } text-${theme === "dark" ? "white" : "gray-800"} `}
       >
         <div className="max-w-6xl mx-auto px-4">
           <div className="flex justify-between">
             <div className="flex space-x-4">
-              {/* Logo */}
-              <div>
+              <div className="flex justify-center items-center ">
+                <Link to="/">
+                  <img
+                    src="/android-chrome-192x192.png"
+                    alt="Logo"
+                    className="w-12 h-12 rounded-full"
+                  />
+                </Link>
+              </div>
+              {/* <div>
                 <a
                   href="#"
                   className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
                 >
                   <span className="font-bold">Brand</span>
                 </a>
-              </div>
+              </div> */}
 
               {/* Primary Nav */}
               <div className="hidden md:flex items-center space-x-1">
-                <a href="#" className="py-5 px-3">
+                {/* <a href="#" className="py-5 px-3">
                   Home
                 </a>
                 <a href="#" className="py-5 px-3">
                   About
-                </a>
+                </a> */}
                 {/* More nav items */}
               </div>
             </div>
@@ -46,11 +56,12 @@ const Navbar: React.FC = () => {
               <input
                 type="text"
                 placeholder="Search"
-                className="px-2 py-1 rounded"
+                className="px-2 py-1 rounded text-black"
               />
-              <button className="px-2 py-1 bg-blue-500 text-white rounded">
+
+              <Button variant="gradient" size="medium">
                 Search
-              </button>
+              </Button>
             </div>
 
             {/* Theme Toggle & Auth Buttons */}
@@ -62,15 +73,10 @@ const Navbar: React.FC = () => {
               </button>
 
               {/* Auth Buttons */}
-              <a href="/login" className="py-2 px-3">
-                Login
-              </a>
-              <a
-                href="/signup"
-                className="py-2 px-3 bg-blue-500 text-white rounded"
-              >
-                Sign Up
-              </a>
+
+              <Button variant="gradient" size="medium">
+                <Link to="/login">Login</Link>
+              </Button>
             </div>
             {/* Mobile Menu Button */}
             <div className="md:hidden flex items-center">
