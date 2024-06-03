f146d08699469577eed7253b888b323cb7604c82
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 08:43:00 +0530

    update axios instance

--- packages/frontend/src/api/axiosInstance.ts
+++ packages/frontend/src/api/axiosInstance.ts
@@ -2,7 +2,24 @@ import axios from "axios";
 import { DOMAIN_URL } from "../utils/constants";
 
 const axiosInstance = axios.create({
-  baseURL: DOMAIN_URL + "/api",
+    baseURL: DOMAIN_URL + "/api",
 });
 
+axiosInstance.interceptors.request.use((config) => {
+    console.log("AXIOS Config", config);
+
+    try {
+        const currentUser = localStorage.getItem("currentUser");
+
+        if (currentUser) {
+            const token = JSON.parse(currentUser).token;
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    } catch (error) {
+        console.log("Error", error);
+        return Promise.reject(error);
+    }
+});
 export default axiosInstance;
