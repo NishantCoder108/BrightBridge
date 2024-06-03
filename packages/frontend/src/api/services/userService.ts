a5e92001c71648be1dabd3fd563c6b8cedd8ebc4
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 08:44:35 +0530

    admin login services

--- packages/frontend/src/api/services/userService.ts
+++ packages/frontend/src/api/services/userService.ts
@@ -2,39 +2,51 @@ import axios, { AxiosError } from "axios";
 import axiosInstance from "../axiosInstance";
 
 export const signupUser = async (email: string, password: string) => {
-  try {
-    const response = await axiosInstance.post("/signup", { email, password });
-    console.log("Signup User", { response });
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const serverError = error as AxiosError<{ message: string }>;
-      console.log({ serverError });
-      if (serverError.response) {
-        throw new Error(
-          serverError.response.data.message ||
-            "An error occurred during signup."
-        );
-      }
+    try {
+        const response = await axiosInstance.post("/signup", {
+            email,
+            password,
+        });
+        console.log("Signup User", { response });
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const serverError = error as AxiosError<{ message: string }>;
+            console.log({ serverError });
+            if (serverError.response) {
+                throw new Error(
+                    serverError.response.data.message ||
+                        "An error occurred during signup."
+                );
+            }
+        }
+        throw new Error("An error occurred during signup.");
     }
-    throw new Error("An error occurred during signup.");
-  }
 };
 
-export const loginUser = async (email: string, password: string) => {
-  try {
-    const response = await axiosInstance.post("/login", { email, password });
-    console.log("Login User", { response });
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const serverError = error as AxiosError<{ message: string }>;
-      if (serverError.response) {
-        throw new Error(
-          serverError.response.data.message || "An error occurred during login."
-        );
-      }
+export const loginUser = async (
+    email: string,
+    password: string,
+    role: boolean
+) => {
+    try {
+        const response = await axiosInstance.post("/admin/login", {
+            email,
+            password,
+            role: role ? "ADMIN" : "USER",
+        });
+        console.log("Login User", { response });
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const serverError = error as AxiosError<{ message: string }>;
+            if (serverError.response) {
+                throw new Error(
+                    serverError.response.data.message ||
+                        "An error occurred during login."
+                );
+            }
+        }
+        throw new Error("An error occurred during login.");
     }
-    throw new Error("An error occurred during login.");
-  }
 };
