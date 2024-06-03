a9940ac1e51d5457828a1b809fcd6d3d4bd1966b
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 12:30:17 +0530

    add axios instance , on request and response for redirect to login page , if error come

--- packages/frontend/src/api/axiosInstance.ts
+++ packages/frontend/src/api/axiosInstance.ts
@@ -5,21 +5,68 @@ const axiosInstance = axios.create({
     baseURL: DOMAIN_URL + "/api",
 });
 
-axiosInstance.interceptors.request.use((config) => {
-    console.log("AXIOS Config", config);
+const baseURL = "http://localhost:5173/";
+// axiosInstance.interceptors.request.use((config) => {
+//     console.log("AXIOS Config", config);
 
-    try {
+//     try {
+//         const currentUser = localStorage.getItem("currentUser");
+//         console.log("1", config);
+//         if (currentUser) {
+//             const token = JSON.parse(currentUser).token;
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         console.log("2", config);
+
+//         return config;
+//     } catch (error) {
+//         console.log("3", config);
+
+//         console.log("Error", error);
+//         return Promise.reject(error);
+//     }
+// });
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
         const currentUser = localStorage.getItem("currentUser");
-
+        console.log("1", config);
         if (currentUser) {
             const token = JSON.parse(currentUser).token;
             config.headers.Authorization = `Bearer ${token}`;
         }
+        console.log("2", config);
 
         return config;
-    } catch (error) {
-        console.log("Error", error);
+    },
+    function (error) {
+        console.log("Axios Response Error", error);
+
+        if (error.response && error.response.status >= 400) {
+            localStorage.removeItem("currentUser");
+            window.location.replace(baseURL);
+        }
+
         return Promise.reject(error);
     }
-});
+);
+
+// Add a response interceptor
+axiosInstance.interceptors.response.use(
+    (response) => response,
+
+    (error) => {
+        console.log("Axios Response Error", error);
+
+        if (error.response && error.response.status >= 400) {
+            localStorage.removeItem("currentUser");
+            window.location.replace(baseURL);
+        }
+
+        Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
