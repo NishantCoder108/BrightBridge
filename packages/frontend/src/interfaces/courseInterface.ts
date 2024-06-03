74fa6ae903fe18dc5917b12250e3edadf5fe1948
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 16:56:28 +0530

    course interface created

---
+++ packages/frontend/src/interfaces/courseInterface.ts
@@ -0,0 +1,9 @@
+export interface ICourseCard {
+    title: string;
+    imageurl?: string;
+    description: string;
+    instructor?: string;
+    price: string;
+    courseId?: string;
+    _id?: string;
+}
