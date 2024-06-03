3eb79276423bccef1575119b1051223830e49083
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 16:55:50 +0530

    Course card implemented to show cards of courses

--- packages/frontend/src/components/common/CourseCard.tsx
+++ packages/frontend/src/components/common/CourseCard.tsx
@@ -1,63 +1,37 @@
 import React from "react";
-import Button from "./Button";
-import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
-import { Link } from "react-router-dom";
-import VideoPlayer from "../pages/VideoPlayerPage";
+import { ICourseCard } from "../../interfaces/courseInterface";
 
-interface CourseCardProps {
-  title: string;
-  imageUrl: string;
-  description: string;
-  instructor: string;
-  price: string;
-  handleVideo: (watchNowHandler: () => void) => void;
-}
-
-const CourseCard: React.FC<CourseCardProps> = ({
-  title,
-  imageUrl,
-  description,
-  instructor,
-  price,
-  handleVideo,
+const CourseCard: React.FC<ICourseCard> = ({
+    title,
+    imageurl,
+    description,
+    instructor,
+    price,
 }) => {
-  const isToken = useSelector((state: RootState) => state.course.token);
-  return (
-    <div className="max-w-xs rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={imageUrl} alt={`Image of ${title}`} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <div className=" ">{description}</div>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {instructor}
-        </span>
-        <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          {price}
-        </span>
-      </div>
-      <div className="px-6 pb-4">
-        {!isToken ? (
-          <Link
-            to="/login"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Enroll Now
-          </Link>
-        ) : (
-          <Button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            variant="gradient"
-            size="small"
-          >
-            <VideoPlayer handleVideo={handleVideo} />
-          </Button>
-        )}
-      </div>
-    </div>
-  );
+    console.log({ imageurl });
+    return (
+        <div className="max-w-xs rounded overflow-hidden shadow-lg ">
+            {imageurl && (
+                <img
+                    className="w-full"
+                    src={imageurl}
+                    alt={`Image of ${title}`}
+                />
+            )}{" "}
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{title}</div>
+                <div className=" ">{description}</div>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {instructor || "Nishant Js"}
+                </span>
+                <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {price}
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default CourseCard;
