18175848acf29d0781e07d5b34374fc2080b127c
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 16:55:31 +0530

    verify admin component update for protected routes

--- packages/frontend/src/components/auth/Admin/VerifyAdmin.tsx
+++ packages/frontend/src/components/auth/Admin/VerifyAdmin.tsx
@@ -2,64 +2,79 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 // import axiosInstance from "../../../api/axiosInstance";
 import { useEffect, useState } from "react";
+import { verifyAdmin } from "../../../api/services/adminService";
+import { useDispatch } from "react-redux";
+import { setVerifiedToken } from "../../../redux/slices/verifySlice";
+import { clearToken } from "../../../redux/slices/authSlice";
 // import { setVerifiedToken } from "../../../redux/slices/verifySlice";
 // import { useDispatch } from "react-redux";
 
 const VerifyAdmin = () => {
     const [isAdmin, setIsAdmin] = useState<boolean>(false);
-    // const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const currUser = localStorage.getItem("currentUser");
 
-    useEffect(() => {
-        if (currUser) {
-            const verifyAdmin = JSON.parse(currUser);
-            if (verifyAdmin.role === "ADMIN") {
-                setIsAdmin(true);
-            } else {
-                setIsAdmin(false);
+    // const
+    // useEffect(() => {
+    //     if (currUser) {
+    //         const verifyAdmin = verifyAdmin();
+    //         if (verifyAdmin.role === "ADMIN") {
+    //             setIsAdmin(true);
+    //         } else {
+    //             setIsAdmin(false);
+    //         }
+    //     }
+    // }, [currUser]);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const fetchVerifyAdmin = async () => {
+        setIsLoading(true);
+        try {
+            const response = await verifyAdmin();
+            console.log({ response });
+
+            if (response && response.status === 200 && response.data) {
+                setIsLoading(false);
+                if (response.data.role === "ADMIN") {
+                    setIsAdmin(true);
+                    dispatch(setVerifiedToken(true));
+                    navigate("/home", { replace: true });
+                } else {
+                    setIsAdmin(false);
+
+                    dispatch(clearToken());
+                    navigate("/", { replace: true });
+                }
             }
+        } catch (error) {
+            setIsLoading(false);
+            console.log({ error });
+            setIsAdmin(false);
+
+            dispatch(clearToken());
+            navigate("/", { replace: true });
+        }
+    };
+
+    useEffect(() => {
+        console.log("Navigating Calling...");
+
+        if (currUser) {
+            console.log("fetching verification...");
+            fetchVerifyAdmin();
+        } else {
+            navigate("/", { replace: true });
         }
     }, [currUser]);
 
-    // const dispatch = useDispatch();
-    // const navigate = useNavigate();
+    console.log({ isAdmin });
 
-    // const fetchVerifyAdmin = async () => {
-    //     setIsLoading(true);
-    //     try {
-    //         const response = await axiosInstance.post("/admin/me");
-    //         console.log({ response });
-
-    //         if (response.status === 200 && response.data) {
-    //             if (response.data.role === "ADMIN") {
-    //                 setIsAdmin(true);
-    //                 dispatch(setVerifiedToken(true));
-    //                 navigate("/home", { replace: true });
-    //             } else {
-    //                 setIsAdmin(false);
-    //                 dispatch(setVerifiedToken(false));
-    //                 navigate("/", { replace: true });
-    //             }
-    //             setIsLoading(false);
-    //         }
-    //     } catch (error) {
-    //         setIsLoading(false);
-    //         console.log({ error });
-    //         navigate("/", { replace: true });
-    //         setIsAdmin(false);
-    //         dispatch(setVerifiedToken(false));
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     fetchVerifyAdmin();
-    // }, []);
-
-    // console.log({ isAdmin });
-
-    // if (isLoading) {
-    //     return <h1>Verifying Admin...</h1>;
-    // }
+    console.log({ isLoading });
+    if (isLoading) {
+        return <h1>Verifying Admin...</h1>;
+    }
 
     return (
         <>
