fd4d736bba4b798856b93eb26baf6b7606d86724
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-03 08:43:42 +0530

    add admin services , fetch all course

---
+++ packages/frontend/src/api/services/adminCourseService.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosError } from "axios";
+import AxiosInstances from "../axiosInstance";
+
+// AxiosInstances.interceptors.request.use((config) => {
+//     console.log("AXIOS Config", config);
+
+//     try {
+//         const currentUser = localStorage.getItem("currentUser");
+
+//         if (!currentUser) {
+//             throw new Error("Token is not available");
+//         }
+
+//         const token = JSON.parse(currentUser).token;
+//         config.headers.Authorization = `Bearer ${token.token}`;
+//         return config;
+//     } catch (error) {
+//         console.log("Error", error);
+//         return Promise.reject(error);
+//     }
+// });
+
+export const getAllCourses = async () => {
+    try {
+        const response = await AxiosInstances.get("/admin/courses");
+        console.log("All Admin Courses", { response });
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const serverError = error as AxiosError<{ message: string }>;
+            console.log({ serverError });
+            if (serverError.response) {
+                throw new Error(
+                    serverError.response.data.message ||
+                        "An error occurred during getting courses."
+                );
+            }
+        }
+        throw new Error("An error occurred during getting courses.");
+    }
+};
