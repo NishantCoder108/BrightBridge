51fa6b45d0e87e98ea3bac7b11b8f37ed6adb305
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-04 17:14:25 +0530

    Refresh Get All courses ,when course creating.

--- packages/frontend/src/components/pages/Admin/AdminAllCoursePage.tsx
+++ packages/frontend/src/components/pages/Admin/AdminAllCoursePage.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { getAllCourses } from "../../../api/services/adminCourseService";
 import CourseCard from "../../common/CourseCard";
 import { ICourseCard } from "../../../interfaces/courseInterface";
 
-const AdminAllCourse = () => {
+interface IProps {
+    refreshData: boolean;
+}
+const AdminAllCourse = memo(({ refreshData }: IProps) => {
     const [courses, setCourses] = useState([]);
-
     const [errCourse, setErrcourse] = useState("");
+
     const fetchAllCourse = async () => {
         try {
             const courses = await getAllCourses();
@@ -24,7 +27,7 @@ const AdminAllCourse = () => {
 
     useEffect(() => {
         fetchAllCourse();
-    }, []);
+    }, [refreshData]);
 
     console.log({ courses });
     return (
@@ -33,18 +36,12 @@ const AdminAllCourse = () => {
                 <div className="flex flex-wrap items-start py-6 gap-6 justify-evenly">
                     {courses.length > 0 &&
                         courses.map((course) => {
-                            const {
-                                courseId,
-                                title,
-                                imageurl,
-                                description,
-                                price,
-                                _id,
-                            } = course as ICourseCard;
+                            const { title, imageurl, description, price, _id } =
+                                course as ICourseCard;
 
                             return (
                                 <CourseCard
-                                    key={courseId}
+                                    key={_id}
                                     courseId={_id}
                                     title={title}
                                     imageurl={imageurl}
@@ -59,6 +56,6 @@ const AdminAllCourse = () => {
             )}
         </div>
     );
-};
+});
 
 export default AdminAllCourse;
