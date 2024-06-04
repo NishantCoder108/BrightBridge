4f8642eb4b6a6cf8c30c9756d083679abfa6574f
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-04 17:15:46 +0530

    Manage component with different components and optimized code

--- packages/frontend/src/components/pages/Admin/AdminHomePage.tsx
+++ packages/frontend/src/components/pages/Admin/AdminHomePage.tsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
+import Button from "../../common/Button";
+import Modal from "../../common/Modal";
 import AdminAllCourse from "./AdminAllCoursePage";
+import AdminCreateCourse from "./AdminCreateCourse";
 
 const AdminHomePage = () => {
+    const [refreshData, setRefreshData] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleModal = () => {
+        setOpenModal(!openModal);
+    };
+
+    const handleRefreshData = (data: boolean) => {
+        setRefreshData(data);
+    };
     return (
         <div>
             <h3>Admin Home Page</h3>
+
             <hr />
-            <AdminAllCourse />
+            <Modal
+                isOpen={openModal}
+                key={"admin_create_course_modal"}
+                onClose={() => setOpenModal(false)}
+                children={
+                    <AdminCreateCourse
+                        handleRefreshData={handleRefreshData}
+                        refreshData={refreshData}
+                    />
+                }
+            />
+
+            <div className="flex flex-col items-center justify-end">
+                <div className="flex items-center justify-end w-full pr-6 py-3">
+                    <Button
+                        onClick={handleModal}
+                        className="border border-red-800 inline-block right-0 "
+                        size="small"
+                    >
+                        Create
+                    </Button>
+                </div>
+
+                <hr />
+                <AdminAllCourse refreshData={refreshData} />
+            </div>
         </div>
     );
 };
