a2c0d2491a43b559096e7a559f11f5651fa7e1f3
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-06-10 15:20:14 +0530

    add routes

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -3,6 +3,10 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/test", (req, res) => {
+    res.send({ message: "successfully deployed.", status: 200 });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server  running on  http://localhost:${PORT}`);
+    console.log(`Server  running on  http://localhost:${PORT}`);
 });
